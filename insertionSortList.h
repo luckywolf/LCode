5c7a70d4e0b1608ff9e5ba541b9bc5e96756c508
luckywolf <zhou4car@gmail.com>
2014-07-29 15:24:09 -0700

    Update insertionSortList.h

--- insertionSortList.h
+++ insertionSortList.h
@@ -8,7 +8,8 @@
  */
 class Solution {
 public:
-    ListNode *insertionSortList(ListNode *head) {
+// first try, is it correct although all test cases pass.
+    ListNode *insertionSortList_1(ListNode *head) {
         if (!head) {
             return NULL;
         }
@@ -25,5 +26,28 @@ public:
             pre->next = mov;
         }
         return dummy.next;
+    }
+
+// second round
+    ListNode *insertionSortList(ListNode *head) {
+        ListNode dummy(INT_MIN); 
+        dummy.next = head;
+        ListNode *cur = &dummy;
+        while (cur->next) {
+            if (cur->next->val >= cur->val)
+                cur = cur->next;
+            else
+                insert(&dummy, cur, cur->next);
+        }
+        return dummy.next;
+    }
+    
+    void insert(ListNode *head, ListNode *tail, ListNode *node) {
+        ListNode *cur = head;
+        while (cur->next->val < node->val)
+            cur = cur->next;
+        tail->next = node->next;
+        node->next = cur->next;
+        cur->next = node;
     }
 };
