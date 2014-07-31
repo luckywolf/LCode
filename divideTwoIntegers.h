574df1ba7b748c238000b64c67a699b53d055a45
luckywolf <zhou4car@gmail.com>
2014-07-31 10:11:58 -0700

    Update divideTwoIntegers.h

--- divideTwoIntegers.h
+++ divideTwoIntegers.h
@@ -50,4 +50,99 @@ public:
         return flag ? res : -res;
     }
     
+    // second round
+    int divide_3(int dividend, int divisor) {
+        assert(divisor != 0);
+        if (dividend == 0) {
+            return 0;
+        }
+        bool positive = dividend > 0 && divisor > 0 ||
+                    dividend < 0 && divisor < 0;
+        long long divdll = abs((long long)dividend);
+        long long divsll = abs((long long)divisor);
+        int res = 0;
+        while (divdll >= divsll) {
+            unsigned int temp = 1;
+            long long tempdivs = divsll;
+            while ((tempdivs << 1) <= divdll) {
+                tempdivs <<= 1;
+                temp <<= 1;
+            }
+            res += temp;
+            divdll -= tempdivs;
+        // code is cleaner with some performance penalty
+            // while(divdll >= divsll)) {
+            //     tempdivs >>= 1;
+            //     temp >>= 1;
+            //     if (divdll >= adivsll) {
+            //         res += temp;
+            //         adivd -= adivs;
+            //     }
+            // }
+        }
+        return positive ? res: -res;
+    }
+    
+    // rewrite
+    int divide_4(int dividend, int divisor) {
+        assert(divisor != 0);
+        if (dividend == 0) {
+            return 0;
+        }
+        bool positive = dividend > 0 && divisor > 0 ||
+                    dividend < 0 && divisor < 0;
+        unsigned int divd = abs(dividend);
+        unsigned int divs = abs(divisor);
+        int res = 0;
+        if (divd >= divs) {
+            int temp = 1;
+            unsigned int tempdivs = divs;
+            while ( (tempdivs << 1) > 0 && (tempdivs << 1) <= divd) {
+                tempdivs <<= 1;
+                temp <<= 1;
+            }
+            res += temp;
+            divd -= tempdivs;
+            while(divd >= divs) {
+                tempdivs >>= 1;
+                temp >>= 1;
+                if (divd >= tempdivs) {
+                    res += temp;
+                    divd -= tempdivs;
+                }
+            }
+        }
+        return positive ? res: -res;
+    }
+    
+    // rewrite
+    int divide(int dividend, int divisor) {
+        assert(divisor != 0);
+        if (dividend  == 0) {
+            return 0;
+        }
+        bool flag = dividend > 0 && divisor > 0 ||
+                    dividend < 0 && divisor < 0;
+        unsigned int divd = abs(dividend);
+        unsigned int divs = abs(divisor);
+        int temp[32] = {0};
+        int res = 0;
+        int i = 0;
+        while (divs > 0 && divs <= divd) {
+            temp[i] = divs;
+            divs <<= 1;
+            i++;
+        }
+        i--;
+        while (i >= 0) {
+            if (divd >= temp[i]) {
+                res += 1 << i;
+                divd -= temp[i];
+            }
+            i--;
+        }
+        return flag ? res : -res;
+    }
+        
+    
 };
