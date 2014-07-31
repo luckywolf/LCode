643eedaf6f195e7b08a1816e24109f6a08698fad
luckywolf <zhou4car@gmail.com>
2014-07-31 11:29:26 -0700

    Update plusOne.h

--- plusOne.h
+++ plusOne.h
@@ -3,15 +3,12 @@ public:
     vector<int> plusOne(vector<int> &digits) {
         int n = digits.size();
         int carry = 1;
-        for (int i = n-1; i >= 0; i--) {
+        for (int i = n-1; i >= 0 && carry; i--) {
             int sum = digits[i] + carry;
             digits[i] = sum % 10;
             carry = sum / 10;
-            if (carry == 0) {
-                break;
-            }
         }
-        if (carry == 1) {
+        if (carry) {
             digits.insert(digits.begin(), 1);
         }
         return digits;
