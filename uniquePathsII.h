bc926f7aa5475b724973fdeca7e09217651832ba
luckywolf <zhou4car@gmail.com>
2014-07-31 13:55:53 -0700

    Update uniquePathsII.h

--- uniquePathsII.h
+++ uniquePathsII.h
@@ -22,4 +22,24 @@ public:
         }
         return dp[m-1][n-1];
     }
+    
+    // second round 
+    int uniquePathsWithObstacles(vector<vector<int> > &obstacleGrid) {
+        int m = obstacleGrid.size();
+        assert(m > 0);
+        int n = obstacleGrid[0].size();
+        assert(n > 0);
+        vector<int> dp(n, 0);
+        dp[0] = obstacleGrid[0][0] == 1 ? 0: 1;
+        for (int i = 1; i < n; ++i) {
+            dp[i] = obstacleGrid[0][i] == 1 ? 0: dp[i-1];
+        }
+        for (int i = 1; i < m; ++i) {
+            dp[0] = obstacleGrid[i][0] == 1 ? 0: dp[0];
+            for (int j = 1; j < n; ++j) {
+                dp[j] = obstacleGrid[i][j] == 1 ? 0 : dp[j] + dp[j-1]; 
+            }
+        }
+        return dp[n-1];
+    }    
 };
