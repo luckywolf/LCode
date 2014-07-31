1c48ba3c2353f2c6d8888bdee3707f2580d145c4
luckywolf <zhou4car@gmail.com>
2014-07-31 13:56:45 -0700

    Update uniquePaths.h

--- uniquePaths.h
+++ uniquePaths.h
@@ -16,4 +16,19 @@ public:
         }
         return dp[m-1][n-1];
     }
+    
+    // second round
+    int uniquePaths(int m, int n) {
+      assert(m >= 0 && n >= 0);
+      if (m == 0 || n == 0) {
+          return 0;
+      }
+      vector<int> dp(n, 1);
+      for (int i = 1; i < m; ++i) {
+          for (int j = 1; j < n; ++j) {
+              dp[j] += dp[j-1];
+          }
+      }
+      return dp[n-1];
+    }    
 };
