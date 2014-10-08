dfa5cb3cc63520de5f2655c78d72ff428bf984e9
luckywolf <zhou4car@gmail.com>
2014-10-07 20:41:44 -0700

    add more solutions

--- c3_longestPalindromicSubstring.h
+++ c3_longestPalindromicSubstring.h
@@ -6,8 +6,8 @@ and there exists one unique longest palindromic substring.
 */
 class Solution {
 public:
-// Time O(n^2), Space O(1)
-    string longestPalindrome(string s) {
+// Space O(1), Time O(n^2)
+    string longestPalindrome_1(string s) {
         int n =  s.size();
         int first = 0, second = 0; // s[0], the first center in the possible 2n-1 centers
         for (int i = 0; i < n; ++i) {
@@ -29,5 +29,26 @@ public:
             }
         }
         return s.substr(first, second - first + 1);
+    }
+
+// Space O(n^2); Time O(n^2), but is better than the above algorithm
+    string longestPalindrome_2(string s) {
+        int n =  s.size();
+        bool dp[n][n];
+        memset(dp, 0, sizeof(dp));
+        int start = 0, end = 0; // s[0]
+        for (int i = 0; i < n; ++i) {
+            dp[i][i] = true;
+        }
+        for (int i = n-2; i >= 0 ; --i) {
+            for (int j = i+1; j < n; ++j) {
+                dp[i][j] = (j - i <= 2 || dp[i+1][j-1]) && s[i] == s[j];
+                if (dp[i][j] && j - i > end - start) {
+                    end = j;
+                    start = i;
+                }
+            }
+        }
+        return s.substr(start, end - start + 1); 
     }
 };
