3d29c90c8a9666cd40f0122bb4fa7448c603c84c
luckywolf <zhou4car@gmail.com>
2014-10-19 18:13:38 -0700

    updated comments

--- a5_3sum.h
+++ a5_3sum.h
@@ -5,7 +5,7 @@ Given an array S of n integers, are there elements a, b, c in S such that a + b 
 Find all unique triplets in the array which gives the sum of zero.
 
 Note:
-Elements in a triplet (a,b,c) must be in non-descending order. (ie, a ¡Ü b ¡Ü c)
+Elements in a triplet (a,b,c) must be in non-descending order. (ie, a <= b <= c)
 The solution set must not contain duplicate triplets.
     For example, given array S = {-1 0 1 2 -1 -4},
 
