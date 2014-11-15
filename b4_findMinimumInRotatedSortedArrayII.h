cba521e04297309a41e77cc6277219530d668464
luckywolf <zhou4car@gmail.com>
2014-11-15 10:56:35 -0800

    updated comments

--- b4_findMinimumInRotatedSortedArrayII.h
+++ b4_findMinimumInRotatedSortedArrayII.h
@@ -14,7 +14,7 @@ Find the minimum element.
 The array may contain duplicates.
 */
 /*
-For case where AL == AM == AR, the minimum could be on AM¡¯s left or right side 
+For case where AL == AM == AR, the minimum could be on AMÂ¡Â¯s left or right side 
 (eg, [1, 1, 1, 0, 1] or [1, 0, 1, 1, 1]). 
 In this case, we could not discard either subarrays and therefore 
 such worst case degenerates to the order of O(n).
@@ -25,6 +25,8 @@ public:
         int n = num.size();
         int left  = 0;
         int right = n - 1;
+        // because there always exists a solution, "left < right" instead of "left <= right" is used
+        // because there exist duplciated elements, num[left] >= num[right] is needed
         while (left < right && num[left] >= num[right]) {
             int mid = left + (right - left) / 2;
             if (num[mid] > num[right]) {
