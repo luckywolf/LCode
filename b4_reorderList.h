c1448b3cfa5bcce2a01a627aa229068aa188bf65
luckywolf <zhou4car@gmail.com>
2014-11-16 20:42:15 -0800

    updated comments

--- b4_reorderList.h
+++ b4_reorderList.h
@@ -2,7 +2,7 @@
 https://oj.leetcode.com/problems/reorder-list/
 
 Given a singly linked list L: L0->L1->...->Ln-1->Ln,
-reorder it to: L0->Ln->L1->Ln-1->L2->Ln-2¡ú...
+reorder it to: L0->Ln->L1->Ln-1->L2->Ln-2->...
 
 You must do this in-place without altering the nodes' values.
 
