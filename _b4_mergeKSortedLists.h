a6421d804613b9edd7454998b2c3c1e879fb98e0
luckywolf <zhou4car@gmail.com>
2014-11-16 19:48:53 -0800

    optimized code

--- _b4_mergeKSortedLists.h
+++ _b4_mergeKSortedLists.h
@@ -24,16 +24,12 @@ public:
     ListNode *mergeTwoLists(ListNode *l1, ListNode *l2) {
         ListNode dummy(0);
         ListNode *cur = &dummy;
+        ListNode **min = NULL;
         while (l1 && l2) {
-            if (l1->val < l2->val) {
-                cur->next = l1;
-                cur = cur->next;
-                l1 = l1->next;
-            } else {
-                cur->next = l2;
-                cur = cur->next;
-                l2 = l2->next;
-            }
+            min = l1->val < l2->val ? &l1 : &l2;
+            cur->next = *min;
+            *min = (*min)->next;
+            cur = cur->next;
         }
         if (l1) {
             cur->next = l1;
